9c6ea21261ab9c6476d637ba4e5fdcbf262dc353
Alexandre Julião <ajuliao-@student.42sp.org.br>
2024-08-22 18:33:14 -0300

    WIP: starting fix norma

--- src/redirect.c
+++ src/redirect.c
@@ -6,63 +6,101 @@
 /*   By: ajuliao- <ajuliao-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 15:32:04 by ajuliao-          #+#    #+#             */
-/*   Updated: 2024/08/20 01:26:25 by ajuliao-         ###   ########.fr       */
+/*   Updated: 2024/08/22 18:31:22 by ajuliao-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_redirect_greater(t_root_f *root)
+char	*ft_strip_quotes(char *word)
 {
-	root->fd = open(root->right->word, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-	if (root->fd == -1)
+	char	*filename;
+
+	if ((word[0] == '"' && word[ft_strlen(word) - 1] == '"') ||
+		(word[0] == '\'' && word[ft_strlen(word) - 1] == '\''))
 	{
-		perror("minishell");
-		return (-1);
+		filename = ft_substr(word, 1, ft_strlen(word) - 2);
 	}
-	if (dup2(root->fd, STDOUT_FILENO) == -1)
-	{
-		perror("minishell");
-		close(root->fd);
-		return (-1);
-	}
-	close(root->fd);
-	return (0);
+	else
+		filename = word;
+	return (filename);
 }
 
 int	ft_redirect_lesser(t_root_f *root)
 {
-	root->fd = open(root->right->word, O_RDONLY, 0);
+	char	*filename;
+
+	filename = ft_strip_quotes(root->right->word);
+	root->fd = open(filename, O_RDONLY, 0);
 	if (root->fd == -1)
 	{
 		perror("minishell");
+		if (filename != root->right->word)
+			free(filename);
 		return (-1);
 	}
 	if (dup2(root->fd, STDIN_FILENO) == -1)
 	{
 		perror("minishell");
 		close(root->fd);
+		if (filename != root->right->word)
+			free(filename);
 		return (-1);
 	}
 	close(root->fd);
+	free(filename);
 	return (0);
 }
 
-int	ft_redirect_doublegreater(t_root_f *root)
+int	ft_redirect_greater(t_root_f *root)
 {
-	root->fd = open(root->right->word, O_WRONLY | O_CREAT | O_APPEND, 0666);
+	char	*filename;
+
+	filename = ft_strip_quotes(root->right->word);
+	root->fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	if (root->fd == -1)
 	{
 		perror("minishell");
+		if (filename != root->right->word)
+			free(filename);
 		return (-1);
 	}
 	if (dup2(root->fd, STDOUT_FILENO) == -1)
 	{
 		perror("minishell");
 		close(root->fd);
+		if (filename != root->right->word)
+			free(filename);
 		return (-1);
 	}
 	close(root->fd);
+	free(filename);
+	return (0);
+}
+
+int	ft_redirect_doublegreater(t_root_f *root)
+{
+	char	*filename;
+
+	filename = ft_strip_quotes(root->right->word);
+	root->fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0666);
+	if (root->fd == -1)
+	{
+		perror("minishell");
+		if (filename != root->right->word)
+			free(filename);
+		return (-1);
+	}
+	if (dup2(root->fd, STDOUT_FILENO) == -1)
+	{
+		perror("minishell");
+		close(root->fd);
+		if (filename != root->right->word)
+			free(filename);
+		return (-1);
+	}
+	close(root->fd);
+	free(filename);
 	return (0);
 }
 
@@ -92,14 +130,17 @@ void	ft_init_redirect(t_mini *data, t_root_f *root)
 	temp_std[0] = dup(STDIN_FILENO);
 	temp_std[1] = dup(STDOUT_FILENO);
 
-	if (ft_redirect(data, root) == -1)
+	if(root->fd < 0)
 	{
-		dup2(temp_std[0], STDIN_FILENO);
-		dup2(temp_std[1], STDOUT_FILENO);
-		close(temp_std[0]);
-		close(temp_std[1]);
-		data->status = 1;
-		return;
+		if (ft_redirect(data, root) == -1)
+		{
+			dup2(temp_std[0], STDIN_FILENO);
+			dup2(temp_std[1], STDOUT_FILENO);
+			close(temp_std[0]);
+			close(temp_std[1]);
+			data->status = 1;
+			return ;
+		}
 	}
 	ft_exec(data, root->left);
 	dup2(temp_std[0], STDIN_FILENO);
